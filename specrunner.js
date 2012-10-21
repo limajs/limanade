a174598db4e489e34e0974f1e8cf313dc3572fa6
Simon Crabtree <simon@simoncrabtree.net>
2012-10-21 19:30:59 +0100

    Changes to allow chrome to open in win32 and Mac OSX

--- specrunner.js
+++ specrunner.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
+var port = 8000;
 var args = process.argv.slice(2);
 
 var http = require("http");
@@ -10,11 +11,33 @@ var io = require("socket.io").listen(server);
 limanade(server, io, {
     specDir: args[0]
 });
-server.listen(8000);
+server.listen(port);
 console.log("Limanade started");
 
 var child_process = require("child_process");
 
 var browserProcess;
-var browserExePath = process.env['LOCALAPPDATA'] + "\\google\\chrome\\application\\chrome.exe";
-child_process.spawn(browserExePath, ['http://localhost:8000/specrunner', '-incognito']);
+var browserExePath;
+if (process.platform === 'win32') {
+    browserExePath = process.env.LOCALAPPDATA + "\\google\\chrome\\application\\chrome.exe";
+    browserProcess = child_process.spawn(browserExePath, [
+        'http://localhost:' + port + '/specrunner',
+        '-incognito'
+    ]);
+} else {
+    browserExePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+    browserProcess = child_process.spawn(browserExePath, [
+        'http://localhost:' + port + '/specrunner',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--disable-default-apps',
+        '--user-data-dir=' + __dirname
+    ]);
+    browserProcess.stdout.on('data', function (data) {
+        console.log(data);
+    });
+    browserProcess.stderr.on('data', function (data) {
+        console.log("Error:", data.toString());
+    });
+}
+console.log("Starting browser", browserExePath);
